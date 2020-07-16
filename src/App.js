7a7aacd47b5971615cbed6014c77675e10eb3823
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-07-16 12:22:03 -0700

    createContext for Products

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './contexts/ProductContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -12,22 +13,23 @@ function App() {
   const [cart, setCart] = useState([]);
 
   const addItem = (item) => {
-    // add the given item to the cart
     setCart([...cart, item]);
   };
 
   return (
     <div className='App'>
-      <Navigation cart={cart} />
+      <ProductContext.Provider value={{ products, cart }}>
+        <Navigation cart={cart} />
 
-      {/* Routes */}
-      <Route exact path='/'>
-        <Products products={products} addItem={addItem} />
-      </Route>
+        {/* Routes */}
+        <Route exact path='/'>
+          <Products products={products} addItem={addItem} />
+        </Route>
 
-      <Route path='/cart'>
-        <ShoppingCart cart={cart} />
-      </Route>
+        <Route path='/cart'>
+          <ShoppingCart cart={cart} />
+        </Route>
+      </ProductContext.Provider>
     </div>
   );
 }
