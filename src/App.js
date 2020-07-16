ebe003aef8d0d9ca796ff27da3cc7635333f70f0
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-07-16 12:15:34 -0700

    added addItem functionality

--- src/App.js
+++ src/App.js
@@ -8,27 +8,28 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-	};
+  const addItem = (item) => {
+    // add the given item to the cart
+    setCart([...cart, item]);
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  return (
+    <div className='App'>
+      <Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+      {/* Routes */}
+      <Route exact path='/'>
+        <Products products={products} addItem={addItem} />
+      </Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
-	);
+      <Route path='/cart'>
+        <ShoppingCart cart={cart} />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
