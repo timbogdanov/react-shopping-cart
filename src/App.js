db6c90dea0688ec9e3d78c32812ba1aa641539f6
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-07-16 12:49:37 -0700

    provide data with cart context

--- src/App.js
+++ src/App.js
@@ -20,16 +20,18 @@ function App() {
   return (
     <div className='App'>
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
+        <CartContext.Provider value={{ cart }}>
+          <Navigation />
 
-        {/* Routes */}
-        <Route exact path='/'>
-          <Products />
-        </Route>
+          {/* Routes */}
+          <Route exact path='/'>
+            <Products />
+          </Route>
 
-        <Route path='/cart'>
-          <ShoppingCart cart={cart} />
-        </Route>
+          <Route path='/cart'>
+            <ShoppingCart />
+          </Route>
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
